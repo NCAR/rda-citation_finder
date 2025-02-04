254fe222bd97292547a53ad8194331ba403ce934
rda-dattore <dattore@ucar.edu>
2025-02-04 09:31:24 -0700

    update usage info

---
+++ src/citation_finder/citefind.py
@@ -0,0 +1,35 @@
+import sys
+
+
+def main():
+    if len(sys.argv[1:]) == 0 or sys.argv[1] == "--help":
+        tool_name = sys.argv[0][sys.argv[0].rfind("/")+1:]
+        print((
+            "usage: {} <DOI_GROUP> [options...]".format(tool_name) + "\n"
+            "usage: {} --help".format(tool_name) + "\n"
+            "usage: {} --show-doi-groups".format(tool_name) + "\n"
+            "\n"
+            "required\n"
+            "DOI_GROUP       doi group for which to get citation statistics\n"
+            "                (see --show-doi-groups)\n"
+            "\n"
+            "options\n"
+            "-d <DOI_DATA>   get citation data for a single DOI only\n"
+            "                DOI_DATA is a delimited string (see -s) containing three items:\n"
+            "                - the DOI\n"
+            "                - the publisher of the DOI\n"
+            "                - the asset type\n"
+            "-k              keep the json files from the APIs (default is to remove them)\n"
+            "--no-works      don't collect information about the citing works\n"
+            "--only-services <SERVICES>\n"
+            "                comma-delimited list of the only services to query (default is\n"
+            "                to query all known services)\n"
+            "--no-services <SERVICES>\n"
+            "                comma-delimited list of services to ignore\n"
+            "-s DELIMITER    delimiter string for DOI_DATA (default is a semicolon)\n"
+        ))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
