ed1a1baa4edc2a35e53c103f9c43d67e2bb0fac4
rda-dattore <dattore@ucar.edu>
2026-01-14 16:14:53 -0700

    add convert_unicodes()

--- src/citation_finder/gscholar.py
+++ src/citation_finder/gscholar.py
@@ -160,16 +160,32 @@ def translation(url):
     return data
 
 
+def convert_unicodes(s):
+    s = (s.replace(r"\u00a0", " ")
+          .replace(r"\u2010", "-")
+          .replace(r"\u2013", "-")
+          .replace(r"\u2014", "-")
+          .replace(r"\u2019", "'")
+    )
+    return s
+
+
 def unicode_escape(s):
     escaped_string = ""
+    escaped = False
     for c in s:
         code = ord(c)
         if code < 0x80:
             escaped_string += c
         elif code < 0xff:
             escaped_string += fr"\u00{code:02x}"
+            escaped = True
         else:
             escaped_string += fr"\u{code:04x}"
+            escaped = True
+
+    if escaped:
+        return convert_unicodes(escaped_string)
 
     return escaped_string
 
