e6cebc4c400d2070e00aa72ff48c618e0ffcdf42
rda-dattore <dattore@ucar.edu>
2026-01-15 15:37:25 -0700

    initial commit

---
+++ src/citation_finder/utils.py
@@ -0,0 +1,126 @@
+def convert_unicodes(s):
+    s = (s.replace(r"\u00a0", " ")
+          .replace(r"\u2010", "-")
+          .replace(r"\u2013", "-")
+          .replace(r"\u2014", "-")
+          .replace(r"\u2019", "'"))
+    return s
+
+
+def unicode_escape(s):
+    escaped_string = ""
+    escaped = False
+    for c in s:
+        code = ord(c)
+        if code < 0x80:
+            escaped_string += c
+        elif code < 0xff:
+            escaped_string += fr"\u00{code:02x}"
+            escaped = True
+        else:
+            escaped_string += fr"\u{code:04x}"
+            escaped = True
+
+    if escaped:
+        return convert_unicodes(escaped_string)
+
+    return escaped_string
+
+
+def add_authors_to_db(author_list, ident, db_conn):
+    cursor = db_conn.cursor()
+    id, id_type = ident
+    seqno = 0
+    do_commit = False
+    for author in author_list:
+        if (((id_type == "DOI" and author['creatorType'] == "author") or
+                (id_type == "ISBN" and author['creatorType'] == "editor"))
+                and 'lastName' in author and len(author['lastName']) > 0):
+            parts = author['firstName'].split()
+            fname = unicode_escape(parts[0])
+            if len(parts) > 1:
+                mname = unicode_escape(parts[1])
+            else:
+                mname = ""
+
+            do_commit = True
+            cursor.execute((
+                    "insert into citation.works_authors values (%s, %s, %s, "
+                    "%s, %s, NULL, %s) on conflict do nothing"),
+                    (id, id_type, unicode_escape(author['lastName']),
+                     fname, mname, seqno))
+            seqno += 1
+
+    if (do_commit):
+        db_conn.commit()
+    else:
+        print("*** NO INSERTABLE AUTHORS for '{}'".format(ident))
+
+
+def inserted_book_works_data(works_data, db_conn) -> bool:
+    try:
+        cursor = db_conn.cursor()
+        cursor.execute((
+            "insert into citation.book_works (isbn, title, publisher) values "
+            "(%s, %s, %s) on conflict on constraint book_works_pkey do update "
+            "set title = case when length(excluded.title) > length(book_works."
+            "title) then excluded.title else book_works.title end, publisher "
+            "= case when length(excluded.publisher) > length(book_works."
+            "publisher) then excluded.publisher else book_works.publisher "
+            "end"),
+            (works_data['ISBN'], works_data['bookTitle'],
+             works_data['publisher']))
+        db_conn.commit()
+    except Exception:
+        return False
+
+    return True
+
+
+def inserted_book_chapter_works_data(works_data, db_conn) -> bool:
+    try:
+        cursor = db_conn.cursor()
+        add_authors_to_db(works_data['creators'], (works_data['ISBN'], "ISBN"),
+                          db_conn)
+        cursor.execute((
+            "insert into citation.book_chapter_works (doi, pages, isbn) "
+            "values (%s, %s, %s) on conflict on constraint "
+            "book_chapter_works_pkey do update set pages = case when length("
+            "excluded.pages) > length(book_chapter_works.pages) then excluded."
+            "pages else book_chapter_works.pages end, isbn = case when length("
+            "excluded.isbn) > length(book_chapter_works.isbn) then excluded."
+            "isbn else book_chapter_works.isbn end"),
+            (works_data['DOI'], works_data['pages'], works_data['ISBN']))
+        db_conn.commit()
+        if not inserted_book_works_data(works_data, db_conn):
+            return False
+
+    except Exception:
+        return False
+
+    return True
+
+
+def inserted_journal_works_data(works_data, db_conn) -> bool:
+    try:
+        cursor = db_conn.cursor()
+        cursor.execute((
+                "insert into citation.journal_works (doi, pub_name, volume, "
+                "pages) values (%s, %s, %s, %s) on conflict on constraint "
+                "journal_works_pkey do update set pub_name = case when length("
+                "excluded.pub_name) > length(journal_works.pub_name) then "
+                "excluded.pub_name else journal_works.pub_name end, volume = "
+                "case when length(excluded.volume) > length(journal_works."
+                "volume) then excluded.volume else journal_works.volume end, "
+                "pages = case when length(excluded.pages) > length("
+                "journal_works.pages) then excluded.pages else journal_works."
+                "pages end"), (works_data['DOI'],
+                               works_data['publicationTitle'],
+                               (works_data['volume'] + "(" +
+                                works_data['issue'] + ")"),
+                               works_data['pages']))
+        db_conn.commit()
+    except Exception:
+        return False
+
+    return True
