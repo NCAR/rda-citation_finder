d9684f22bca113c7117e92a25195c250f89e1cb5
rda-dattore <dattore@ucar.edu>
2026-03-25 16:44:00 -0600

    raise exception if cache directory doesn't exist

--- src/citation_finder/cache.py
+++ src/citation_finder/cache.py
@@ -6,6 +6,9 @@ from .local_settings import config
 
 def clean_cache():
     directory = f"{config['temporary-directory-path']}/citation_cache"
+    if not os.path.exists(directory):
+        raise FileNotFoundError(f"cache directory {directory} does not exist")
+
     days_to_keep = 180
     cutoff = time.time() - (days_to_keep * 86400)
     for file in os.listdir(directory):
