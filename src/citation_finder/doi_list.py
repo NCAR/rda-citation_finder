55c3c0496fc03b7b365f4bb88f6df7ab99a4d676
rda-dattore <dattore@ucar.edu>
2026-03-26 15:32:41 -0600

    add code for DOI list from DB

--- src/citation_finder/doi_list.py
+++ src/citation_finder/doi_list.py
@@ -1,9 +1,19 @@
+import psycopg2
+
 from .local_settings import config
 
 
 def get_doi_list_from_db(doi_group):
-    doi_list = []
-    return doi_list
+    try:
+        db = config['citation-database']
+        conn = psycopg2.connect(user=db['user'], password=db['password'],
+                                host=db['host'], dbname=db['dbname'])
+        cursor = conn.cursor()
+        cursor.execute(config['doi-groups'][doi_group]['doi-query']['db'])
+        return [e[0] for e in cursor.fetchall()]
+    finally:
+        if 'conn' in locals():
+            conn.close()
 
 
 def get_doi_list_from_api(doi_group):
