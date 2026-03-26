4af9a6542dd3f6771c08c5ca9566dfbb46201fe7
rda-dattore <dattore@ucar.edu>
2026-03-26 16:39:43 -0600

    return tuples of (DOI, publisher, asset-type) from API

--- src/citation_finder/doi_list.py
+++ src/citation_finder/doi_list.py
@@ -18,17 +18,42 @@ def get_doi_list_from_db(doi_group):
             conn.close()
 
 
+def get_nodes(json_path):
+    nodes = json_path.split(".")
+    if nodes[0] == "$":
+        return nodes[1:]
+    else:
+        raise ValueError(f"'{json_path}' is not a valid JSON path")
+
+
+def decode_response(response, nodes):
+    vals = []
+    o = json.loads(response.text)
+    for x in range(0, len(nodes)):
+        if nodes[x].endswith("[*]"):
+            for entry in o[nodes[x][:-3]]:
+                for y in range(x+1, len(nodes)):
+                    entry = entry[nodes[y]]
+
+                vals.append(entry)
+
+            break
+
+        else:
+            o = o[nodes[x]]
+
+    if len(vals) == 0:
+        vals.append(o)
+
+    return vals
+
+
 def get_doi_list_from_api(doi_group):
-    doi_list = []
     api = config['doi-groups'][doi_group]['doi-query']['api']
     base_url = api['url']
-    doi_response = api['response']['doi'].split(".")
-    if doi_response[0] == "$":
-        del doi_response[0]
-    else:
-        raise ValueError(
-                f"'{api['response']['doi']}' is not a valid JSON path")
-
+    doi_nodes = get_nodes(api['response']['doi'])
+    publisher_nodes = get_nodes(api['response']['publisher'])
+    asset_type_nodes = get_nodes(api['response']['asset-type'])
     page_count = 1
     page_number = 1
     while page_number <= page_count:
@@ -44,24 +69,11 @@ def get_doi_list_from_api(doi_group):
 
         print(url)
         response = requests.get(url)
-        o = json.loads(response.text)
-        for x in range(0, len(doi_response)):
-            if doi_response[x].endswith("[*]"):
-                for entry in o[doi_response[x][:-3]]:
-                    for y in range(x+1, len(doi_response)):
-                        entry = entry[doi_response[y]]
+        dois = decode_response(response, doi_nodes)
+        publishers = decode_response(response, publisher_nodes)
+        asset_types = decode_response(response, asset_type_nodes)
 
-                    doi_list.append(entry)
-
-                break
-
-            else:
-                o = o[doi_response[x]]
-
-        if len(doi_list) == 0:
-            doi_list.append(o)
-
-    return doi_list
+    return list(zip(dois, publishers, asset_types))
 
 
 def get_doi_list(doi_group):
