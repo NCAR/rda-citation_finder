bbdc2fb0e7bcda5165ccd47e5eb3da0472ba74b8
rda-dattore <dattore@ucar.edu>
2026-03-26 15:14:45 -0600

    create helper functions

--- src/citation_finder/doi_list.py
+++ src/citation_finder/doi_list.py
@@ -1,10 +1,20 @@
 from .local_settings import config
 
 
+def get_doi_list_from_db(doi_group):
+    doi_list = []
+    return doi_list
+
+
+def get_doi_list_from_api(doi_group):
+    doi_list = []
+    return doi_list
+
+
 def get_doi_list(doi_group):
     if 'db' in config['doi-groups'][doi_group]['doi-query']:
-        pass
+        return get_doi_list_from_db(doi_group)
     elif 'api' in config['doi-groups'][doi_group]['doi-query']:
-        pass
+        return get_doi_list_from_api(doi_group)
     else:
         raise RuntimeError(f"unable to build list of DOIs for {doi_group}")
