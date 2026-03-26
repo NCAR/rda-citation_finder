50cda3e35206fa9c44acfed4d47477856b361042
rda-dattore <dattore@ucar.edu>
2026-03-26 16:16:29 -0600

    get dois from response

--- src/citation_finder/doi_list.py
+++ src/citation_finder/doi_list.py
@@ -1,4 +1,6 @@
+import json
 import psycopg2
+import requests
 
 from .local_settings import config
 
@@ -20,6 +22,13 @@ def get_doi_list_from_api(doi_group):
     doi_list = []
     api = config['doi-groups'][doi_group]['doi-query']['api']
     base_url = api['url']
+    doi_response = api['response']['doi'].split(".")
+    if doi_response[0] == "$":
+        del doi_response[0]
+    else:
+        raise ValueError(
+                f"'{api['response']['doi']}' is not a valid JSON path")
+
     page_count = 1
     page_number = 1
     while page_number <= page_count:
@@ -34,6 +43,23 @@ def get_doi_list_from_api(doi_group):
             page_number += 1
 
         print(url)
+        response = requests.get(url)
+        o = json.loads(response.text)
+        for x in range(0, len(doi_response)):
+            if doi_response[x].endswith("[*]"):
+                for entry in o[doi_response[x][:-3]]:
+                    for y in range(x+1, len(doi_response)):
+                        entry = entry[doi_response[y]]
+
+                    doi_list.append(entry)
+
+                break
+
+            else:
+                o = o[doi_response[x]]
+
+        if len(doi_list) == 0:
+            doi_list.append(o)
 
     return doi_list
 
