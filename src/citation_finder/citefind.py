f59125001563d670c90949c159ffcb332118c324
rda-dattore <dattore@ucar.edu>
2026-03-27 09:33:51 -0600

    add missing single quote

--- src/citation_finder/citefind.py
+++ src/citation_finder/citefind.py
@@ -67,7 +67,7 @@ def parse_args(args):
         else:
             raise ValueError(
                     f"'{doi_data}' not in proper format (delimiter is "
-                    f"'{settings['delimiter']})")
+                    f"'{settings['delimiter']}')")
 
     return settings
 
