83ab24904ba4fc12891426d252be41577e3deb2e
rda-dattore <dattore@ucar.edu>
2026-04-01 16:02:52 -0600

    added inserted_doi_data()

--- src/citation_finder/inserts.py
+++ src/citation_finder/inserts.py
@@ -32,3 +32,25 @@ def insert_source(works_doi, data_doi, service, **kwargs):
         kwargs['output'].write(
                 "Error while inserting {} source ({}, {}): '{}'\n"
                 .format(service, works_doi, data_doi, err))
+
+
+def inserted_doi_data(data_doi, publisher, asset_type, **kwargs):
+    try:
+        cursor = kwargs['conn'].cursor()
+        cursor.execute(
+                f"insert into {config['citation-database']['schemaname']}."
+                "doi_data (doi_data, publisher, asset_type) values "
+                "(%s, %s, %s) on conflict on constraint doi_data_pkey do "
+                "update set publisher = case when length(excluded.publisher) "
+                "> length(doi_data.publisher) then excluded.publisher else "
+                "doi_data.publisher end, asset_type = case when length("
+                "excluded.asset_type) > length(doi_data.asset_type) then "
+                "excluded.asset_type else doi_data.asset_type end",
+                (data_doi, publisher, asset_type))
+        kwargs['conn'].commit()
+        return True
+    except Exception as err:
+        kwargs['output'].write(
+                "Error updating DOI data ({}, {}, {}): '{}'\n"
+                .format(data_doi, publisher, asset_type, err))
+        return False
