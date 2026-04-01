820f58a73be15f1be12d9f64e3c164343e552e4b
rda-dattore <dattore@ucar.edu>
2026-04-01 14:36:42 -0600

    fix f-string

---
+++ src/citation_finder/inserts.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+from .local_settings import config
+
+
+def inserted_citation(doi, works_doi, service, **kwargs):
+    try:
+        db = config['citation-database']
+        conn = psycopg2.connect(user=db['user'], password=db['password'],
+                                host=db['host'], dbname=db['dbname'])
+        cursor = conn.cursor()
+        cursor.execute(
+                f"insert into {db['schemaname']}."
+                f"{config['doi-groups'][kwargs['doi_group']]['db-table']} "
+                "(doi_data, doi_work, new_flag) values (%s, %s, %s) on "
+                "conflict on constraint (doi_data, doi_work) do nothing",
+                (doi, works_doi, "1"))
+        conn.commit()
+        return True
+    except Exception as err:
+        kwargs['output'].write(
+                "Error while inserting {} citation ({}, {}): '{}'\n"
+                .format(service, doi, works_doi, err))
+        return False
