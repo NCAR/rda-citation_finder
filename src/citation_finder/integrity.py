4890f4cb51b21fea2b8d9c94f7d425fa90e5dfea
rda-dattore <dattore@ucar.edu>
2026-04-09 11:42:09 -0600

    add checks

--- src/citation_finder/integrity.py
+++ src/citation_finder/integrity.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from .utils import db_connect
 
 
@@ -7,8 +9,95 @@ def run_integrity_checks(**kwargs):
         err = f"***DATABASE ERROR from run_integrity_checks(): '{err}'"
         raise RuntimeError(err)
 
+    cursor = conn.cursor()
     # check for empty titles
+    try:
+        cursor.execute(
+                f"select * from {kwargs['schemaname']}.citation.works where "
+                "title = ''")
+        kwargs['mail_message'].write(
+                f"  # works without a title: {len(cursor.fetchall())}\n")
+    except Exception as err:
+        kwargs['mail_message'].write(
+                f"  **Error checking for empty titles: '{err}'\n")
+
     # check author names
+    try:
+        cursor.execute(
+                "select * from citation.works_authors where last_name like "
+                "'%\\\\-%'")
+        kwargs['mail_message'].write(
+                "  # author last names with an escaped hyphen: "
+                f"{len(cursor.fetchall())}\n")
+    except Exception as err:
+        kwargs['mail_message'].write(
+                "  **Error checking for author last names with escaped "
+                f"hyphen: '{err}'\n")
+
+    try:
+        cursor.execute(
+                "select * from citation.works_authors where last_name like "
+                "'%\\\\''%'")
+        kwargs['mail_message'].write(
+                "  # author last names with an escaped apostrophe: "
+                f"{len(cursor.fetchall())}\n")
+    except Exception as err:
+        kwargs['mail_message'].write(
+                "  **Error checking for author last names with escaped "
+                f"apostrophe: '{err}'\n")
+
     # check for missing authors
+    try:
+        cursor.execute(
+                "select w.doi, count(a.last_name) from "
+                f"{kwargs['schemaname']}.works as w left join citation."
+                "works_authors as a on a.id = w.doi group by w.doi having "
+                "count(a.last_name) = 0")
+        res = cursor.fetchall()
+        kwargs['mail_message'].write(
+                f"  # of DOIs with missing authors: {len(res)}\n"
+                "    DOI list:\n")
+        for e in res:
+            kwargs['mail_message'].write(f"      {e[0]}\n")
+
+    except Exception as err:
+        kwargs['mail_message'].write(
+                  f"**Error checking for missing authors: '{err}'\n")
+
     # check for bad publication months
+    try:
+        cursor.execute(
+                f"select * from {kwargs['schemaname']}.works where pub_month "
+                "= 0")
+        kwargs['mail_message'].write(
+                "  # works without a publication month: "
+                f"{len(cursor.fetchall())}\n")
+    except Exception as err:
+        kwargs['mail_message'].write(
+                f"  **Error checking for missing publication months: '{err}'"
+                "\n")
+
+    curr_yrmo = datetime.now().strftime("%Y%m")
+    try:
+        cursor.execute(
+                f"select * from {kwargs['schemaname']}.works where "
+                "pub_year*100+pub_month > %s", (curr_yrmo, ))
+        kwargs['mail_message'].write(
+                "  # works with a future publication month: "
+                f"{len(cursor.fetchall())}\n")
+    except Exception as err:
+        kwargs['mail_message'].write(
+                f"  **Error checking for future publication months: '{err}'\n")
     # print the publisher list
+    try:
+        cursor.execute(
+                f"select distinct publisher from {kwargs['schemaname']}.works")
+        res = cursor.fetchall()
+        kwargs['mail_message'].write("Current Publisher List:\n")
+        for e in res:
+            kwargs['mail_message'].write(f"Publisher: '{e[0]}'\n")
+
+    except Exception as err:
+        kwargs['mail_message'].write(
+                "  **Error getting list of pubishers from 'works' table: "
+                f"'{err}'\n")
