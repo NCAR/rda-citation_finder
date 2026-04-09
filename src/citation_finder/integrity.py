d5d30fc0ed666e15d145f26c2ba31b2d4e470f4e
rda-dattore <dattore@ucar.edu>
2026-04-09 06:10:14 -0600

    add check sections

--- src/citation_finder/integrity.py
+++ src/citation_finder/integrity.py
@@ -3,3 +3,12 @@ from .utils import db_connect
 
 def run_integrity_checks(**kwargs):
     conn, err = db_connect()
+    if conn is None:
+        err = f"***DATABASE ERROR from run_integrity_checks(): '{err}'"
+        raise RuntimeError(err)
+
+    # check for empty titles
+    # check author names
+    # check for missing authors
+    # check for bad publication months
+    # print the publisher list
