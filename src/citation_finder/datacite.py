359dd0ac98f8a2c790482979b9561e4f72c04001
rda-dattore <dattore@ucar.edu>
2026-04-10 08:50:26 -0600

    initial commit

---
+++ src/citation_finder/datacite.py
@@ -0,0 +1,55 @@
+import json
+import os
+import requests
+
+from pathlib import Path
+
+from .inserts import insert_citation
+from .local_settings import config
+from .utils import db_connect, reset_new_flag
+
+
+API_URL = "https://api.datacite.org/dois"
+
+
+def find_citations(**kwargs):
+    kwargs['conn'], err = db_connect()
+    if kwargs['conn'] is None:
+        err = f"***DATABASE ERROR from crossref.find_citations(): '{err}'"
+        raise RuntimeError(err)
+
+    for doi, publisher, asset_type in kwargs['doi_list']:
+        kwargs['output'].write(
+                f"    querying DOI '{doi} | {publisher} | {asset_type}' ...\n")
+        filename = doi.replace("/", "@@") + ".datacite.json"
+        filename = os.path.join(config['temporary-directory-path'], filename)
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                j = json.load(f)
+
+        else:
+            try:
+                response = requests.get(f"{API_URL}/{doi}")
+                j = json.loads(response.text)
+                with open(filename, "w") as f:
+                    f.write(response.text)
+
+            except Exception:
+                Path(filename).unlink(missing_ok=True)
+                kwargs['output'].write(
+                        f"Error reading DataCITE JSON for DOI '{doi}'\n")
+                continue
+
+        if ('citations' not in j['data'] or 'data' not in j['citations'] or
+                len(j['citations']['data']) == 0):
+            continue
+
+        kwargs['output'].write(
+                f"      {len(j['citations']['data'])} citations found ...\n")
+        for work_doi in j['citations']['data']:
+            work_doi = work_doi['id']
+            success, new_entry = insert_citation(
+                    doi, work_doi, "DataCite", **kwargs)
+
+    reset_new_flag(**kwargs)
+    kwargs['conn'].close()
