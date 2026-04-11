cf91e87a63b09e9bdd453b76679e218ccfdf7872
rda-dattore <dattore@ucar.edu>
2026-04-10 19:21:59 -0600

    update pubname for preprints

--- src/citation_finder/crossref.py
+++ src/citation_finder/crossref.py
@@ -137,7 +137,11 @@ def insert_publication_data(work_data, **kwargs):
              work_data['message']['subtype'] == "preprint")):
         if ('container-title' not in work_data['message'] or
                 len(work_data['message']['container-title']) == 0):
-            pubname = work_data['message']['short-container-title'][0]
+            if 'short-container-title' in work_data['message']:
+                pubname = work_data['message']['short-container-title'][0]
+            else:
+                pubname = work_data['message']['institution'][0]['name']
+
         else:
             pubname = work_data['message']['container-title'][0]
 
