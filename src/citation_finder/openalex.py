b143c341796be0d46a641465cb25e24572c42af1
rda-dattore <dattore@ucar.edu>
2026-04-28 20:47:14 -0600

    initial commit

---
+++ src/citation_finder/openalex.py
@@ -0,0 +1,73 @@
+import json
+import os
+import requests
+
+from pathlib import Path
+
+from .local_settings import config
+from .utils import db_connect
+
+
+API_URL = "https://api.openalex.org/works"
+
+
+def find_citations(**kwargs):
+    kwargs['conn'], err = db_connect()
+    if kwargs['conn'] is None:
+        err = f"***DATABASE ERROR from crossref.find_citations(): '{err}'"
+        raise RuntimeError(err)
+
+    for doi, publisher, asset_type in kwargs['doi_list']:
+        kwargs['output'].write(
+                f"    querying DOI '{doi} | {publisher} | {asset_type}' ...\n")
+        # get the OpenAlex ID for the DOI
+        filename = doi.replace("/", "@@") + ".openalex_id.json"
+        filename = os.path.join(config['temporary-directory-path'], filename)
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                j = json.load(f)
+
+        else:
+            try:
+                response = requests.get(
+                        f"{API_URL}/https://doi.org/{doi}?select=id&api_key="
+                        f"{config['services']['openalex']['api-key']}")
+                j = json.loads(response.text)
+                with open(filename, "w") as f:
+                    f.write(response.text)
+
+            except Exception:
+                Path(filename).unlink(missing_ok=True)
+                kwargs['output'].write(
+                        f"Error reading DataCITE JSON for DOI '{doi}'\n")
+                continue
+
+        if 'id' not in j:
+            continue
+
+        openalex_id = j['id']
+        kwargs['output'].write(f"      OpenAlex ID: '{openalex_id}'\n")
+        params = {'per_page': 100, 'page': 1,
+                  'filter': (
+                          "cites:{openalex_id},type:book-chapter|article|"
+                          "preprint"),
+                  'select': (
+                          "doi,type,title,publication_date,primary_location,"
+                          "authorships"),
+                  'api_key': config['services']['openalex']['api-key']}
+        count = 0x7fffffff
+        num_results = 0
+        while num_results < count:
+            count = 0
+            filename = (doi.replace("/", "@@") + ".openalex." +
+                        str(params['page']) + ".json")
+            filename = os.path.join(config['temporary-directory-path'],
+                                    filename)
+            if os.path.exists(filename):
+                with open(filename, "r") as f:
+                    j = json.load(f)
+
+            else:
+                pass
+
+            params['page'] += 1
