ffcac0e0ac2797bdabd52ddb2501889e89bf6880
rda-dattore <dattore@ucar.edu>
2026-05-04 14:26:57 -0600

    add work from CrossRef if exists, else WoS

--- src/citation_finder/wos.py
+++ src/citation_finder/wos.py
@@ -3,26 +3,116 @@ import os
 import requests
 import time
 
+from .crossref import get_work_data as get_crossref_work_data
+from .crossref import get_publication_date as get_crossref_publication_date
+from .crossref import insert_authors as insert_crossref_authors
+from .crossref import (
+        insert_publication_data as insert_crossref_publication_data)
+from .inserts import (insert_citation, insert_general_work_data,
+                      insert_journal_work_data, insert_source,
+                      insert_work_author, inserted_doi_data)
 from .local_settings import config
+from .utils import convert_unicodes, db_connect, repair_string, verified_DOI
 
 
 API_URL = "https://wos-api.clarivate.com/api/wos"
 
 
-def process_works_id(works_id, **kwargs):
-    headers = {'X-ApiKey': config['services']['wos']['api-key']}
-    params = {'databaseId': "WOS", 'count': 1, 'firstRecord': 1,
-              'viewField': "identifiers"}
-    if not kwargs['no_works']:
-        params['viewField'] += "+names+titles+pub_info"
+def process_work(work_doi, work, **kwargs):
+    work_data = get_crossref_work_data(work_doi)
+    if work_data is not None:
+        # add author data for the citing work
+        insert_crossref_authors(work_data, **kwargs)
+        # add type-specific data for the work
+        pubtype = insert_crossref_publication_data(work_data, **kwargs)
+        if pubtype is not None:
+            # add general data about the work
+            message = work_data['message']
+            pubdate = get_crossref_publication_date(message, **kwargs)
+            if len(pubdate) > 0:
+                title = convert_unicodes(repair_string(message['title'][0]))
+                insert_general_work_data(
+                        work_doi, title, pubdate, pubtype,
+                        message['publisher'], **kwargs)
+                if pubdate['month'] == 0:
+                    kwargs['output'].write(
+                            "        Warning: missing publication month for "
+                            f"work DOI {work_doi} citing {kwargs['doi']}\n")
 
-    # get the data for each work
+                return
+
+    if 'pubdate' in locals():
+        title = convert_unicodes(repair_string(message['title'][0]))
+        insert_general_work_data(
+                work_doi, title,
+                {'year': work['static_data']['summary']['pub_info']['pubyear'],
+                 'month': 0},
+                pubtype, message['publisher'], **kwargs)
+        kwargs['output'].write(
+                "        Warning: missing publication month for work DOI "
+                f"{work_doi} citing {kwargs['doi']}\n")
+        return
+
+    if 'pubtype' in locals():
+        kwargs['output'].write(
+                "        ***No CrossRef publication type - WoS publication "
+                "type: "
+                f"{work['static_data']['summary']['pub_info']['pubtype']}\n")
+        return
+
+    if work['static_data']['summary']['pub_info']['pubtype'] == "Journal":
+        try:
+            for name in work['static_data']['summary']['names']['name']:
+                names = name['display_name'].split(",")
+                author = {'family': names[0]}
+                names = names[1].split()
+                author['given'] = names[0].strip()
+                if len(names) > 1:
+                    author['middle'] = " ".join([e.strip() for e in names[1:]])
+
+                insert_work_author(work_doi, author, name['seq_no']-1,
+                                   **kwargs)
+
+        except Exception as err:
+            kwargs['output'].write(
+                    f"***Error adding authors for WOS work {work['UID']}: "
+                    f"'{err}'\n")
+            return
+
+        summary = work_data['static_data']['summary']
+        titles = summary['pub_info']['titles']
+        for title in titles:
+            if title['type'] == "source":
+                pubname = title['content']
+            elif title['type'] == "item":
+                work_title = title['content']
+
+        insert_journal_work_data(work_doi, pubname, summary['pub_info']['vol'],
+                                 "", **kwargs)
+        insert_general_work_data(
+                work_doi, work_title,
+                {'year': summary['pub_info']['pubyear'], 'month': 0},
+                "J", "", **kwargs)
+        kwargs['output'].write(
+                "        Warning: missing publication month and publisher "
+                f"for work DOI {work_doi} citing {kwargs['doi']}\n")
+        return
+
+    kwargs['output'].write(
+            "        ***No CrossRef publication type and unknown WoS "
+            "publication type: "
+            f"{work['static_data']['summary']['pub_info']['pubtype']}\n")
 
 
 def find_citations(**kwargs):
+    kwargs['conn'], err = db_connect()
+    if kwargs['conn'] is None:
+        err = f"***DATABASE ERROR from wos.find_citations(): '{err}'"
+        raise RuntimeError(err)
+
     headers = {'X-ApiKey': config['services']['wos']['api-key']}
     wos_id_params = {'databaseId': "DCI", 'count': 1, 'firstRecord': 1,
-                     'viewField': "none"}
+                     'viewField': "none", 'optionView': "SR"}
     for doi, publisher, asset_type in kwargs['doi_list']:
         kwargs['output'].write(
                 f"    querying DOI '{doi} | {publisher} | {asset_type}' ...\n")
@@ -62,9 +152,9 @@ def find_citations(**kwargs):
 
         kwargs['output'].write(f"      WoS ID: '{wos_id}'\n")
         # get the WoS IDs for the "works" that have cited this DOI
-        works_ids = []
         works_id_params = {'databaseId': "WOS", 'uniqueId': wos_id,
-                           'count': 100, 'firstRecord': 1, 'viewField': ""}
+                           'count': 100, 'firstRecord': 1, 'viewField': "",
+                           'optionView': "SR"}
         num_records = 2
         while works_id_params['firstRecord'] < num_records:
             time.sleep(0.6)
@@ -80,13 +170,47 @@ def find_citations(**kwargs):
                 works_id_params['firstRecord'] = num_records
                 continue
 
-            for id in j['Data']['Records']['records']['REC']:
-                works_ids.append(id['UID'])
+            for work in j['Data']['Records']['records']['REC']:
+                work_doi = None
+                try:
+                    for identifier in (work['dynamic_data']['cluster_related']
+                                       ['identifiers']['identifier']):
+                        if identifier['type'] == "doi":
+                            work_doi = identifier['value']
+
+                except Exception:
+                    pass
+
+                if work_doi is None:
+                    kwargs['output'].write(
+                            "***Unable to get DOI for WOS work "
+                            f"{work['UID']}\n")
+                    continue
+
+                is_valid_doi = verified_DOI(work_doi, **kwargs)
+                if not is_valid_doi:
+                    kwargs['output'].write(
+                            f"Info: ignoring invalid DOI '{work_doi}' for "
+                            f"WOS work {work['UID']}\n")
+                    continue
+
+                # insert the work doi and source
+                success, new_entry = insert_citation(
+                        doi, work_doi, "WoS", **kwargs)
+                if not success:
+                    continue
+
+                insert_source(work_doi, doi, "WoS", **kwargs)
+                if not inserted_doi_data(doi, publisher, asset_type, **kwargs):
+                    continue
+
+                if kwargs['no_works'] or not new_entry:
+                    continue
+
+                process_work(work_doi, work, doi=doi, **kwargs)
 
             num_records = j['QueryResult']['RecordsFound']
             works_id_params['firstRecord'] += works_id_params['count']
 
         kwargs['output'].write(
-                f"        {len(works_ids)} citations found ...\n")
-        for works_id in works_ids:
-            process_works_id(works_id, **kwargs)
+                f"        {num_records} citations found ...\n")
